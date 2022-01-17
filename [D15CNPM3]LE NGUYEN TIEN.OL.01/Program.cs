4a8bd7bf248ba90cad05abc7c6021c855fe6353b
NguyenTien3102 <letien31082002@gmail.com>
2022-01-17 11:15:27 +0700

    .

---
+++ [D15CNPM3]LE NGUYEN TIEN.OL.01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _D15CNPM3_LE_NGUYEN_TIEN.OL._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // chuong trinh giai phuong trinh bac nhat ax + b = 0 voi ax + b = 0 
+
+            double a, b, x;
+
+            Console.Write("Nhap a = "); a = Double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap b = "); b = Double.Parse(Console.ReadLine());
+
+            if (a == 0)
+
+            {
+
+                if (b == 0) Console.WriteLine("Phuong trinh co vo so nghiem");
+
+                else Console.WriteLine("Phuong trinh vo nghiem");
+
+            }
+
+            else
+
+            {
+
+                x = (-b / a);
+
+                Console.WriteLine("Phuong trinh co nghiem duy nhat x = " + x);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
