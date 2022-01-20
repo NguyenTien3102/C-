cb4e294c76bee3cb4a4c5c14b03ca9c93f8240e2
NguyenTien3102 <letien31082002@gmail.com>
2022-01-20 11:43:35 +0700

    LE NGUYEN TIEN

---
+++ LE NGUYEN TIEN.OL.03.T/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LE_NGUYEN_TIEN.OL._03.T
+{
+    class Program
+    {
+        public static void main()
+        {
+            int[] arr;
+            int n;
+            Console.Write(" nhap n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            arr = new int[n];
+            // nhap phan tu cho array
+            for ( int i=0; i<arr.Length; i++)
+            {
+                Console.Write($"arr[{i} = ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            // arr = arr.OrderBy(x => x).ToArray
+            {
+                if (n == 2)
+                    Console.Write("so da cho la so nguyen to");
+                else
+                    Console.Write("so da cho khong phai la so nguy to");
+            }
+            // xuat phan tu cho array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
